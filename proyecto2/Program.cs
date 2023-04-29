4a536404fe100466bb672a6e6f61c3b7efaae1fd
Débora Hernández <debora.hgk6@gmail.com>
2023-04-29 10:01:39 -0500

    Agregar archivos de proyecto.

---
+++ proyecto2/Program.cs
@@ -0,0 +1,165 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using proyecto2;
+using System.Reflection;
+string cont;
+int turno1 = 0;
+int turno2 = 0;
+juego juego = new juego();
+bool victoria = false;
+string ganador = null;
+List<string> registroGanadores = new List<string>();
+do
+{
+    Console.Clear();
+    Console.WriteLine("MENU");
+    Console.WriteLine("1. Jugar");
+    Console.WriteLine("2. Tabla de ganadores");
+    Console.WriteLine("3. Cerrar programa");
+    int op = Convert.ToInt32(Console.ReadLine());
+    
+    while (op > 3 || op < 0)
+    {
+        Console.WriteLine("La opcion ingresada no es valida, intentelo de nuevo");
+        op = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Clear();
+    switch (op)
+    {
+        case 1:
+
+            string nombre;
+            string nombre2;
+
+            Console.WriteLine("BIENVENIDO");
+            Console.WriteLine("Cuantas personas jugaran?");
+            int per = Convert.ToInt32(Console.ReadLine());
+
+            while (per != 1 && per != 2)
+            {
+                Console.WriteLine("ERROR: jugara 1 o 2 personas?");
+                per = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (per)
+            {
+                case 1:
+
+                    Console.WriteLine("Ingrese su nombre");
+                    nombre = Console.ReadLine();
+
+                    juego.GraficarTablero();
+                    while (victoria == false)
+                    {
+                        Console.WriteLine("Es el turno de " + nombre);
+                        turno1++;
+                        juego.jugador1();
+                        victoria = juego.Victoria1();
+                        Console.Clear();
+                        juego.GraficarTablero();
+                        if (victoria == true)
+                        {
+                            Console.WriteLine("GANÓ " + nombre);
+                            Console.WriteLine("Has ganado en " + turno1 + " turnos");
+                            ganador = nombre;
+                            break;
+                        }
+                        Console.WriteLine("Es el turno de computadora");
+                        juego.computadora();
+                        victoria = juego.Victoria2();
+                        juego.GraficarTablero();
+                        if (victoria == true)
+                        {
+                            Console.WriteLine("GANÓ COMPUTADORA");
+                            break;
+                        }
+                    }
+                    if (ganador != null)
+                    {
+                        registroGanadores.Add(ganador);
+                    }
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Ingrese el nombre del jugador 1");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese el nombre del jugador 2");
+                    nombre2 = Console.ReadLine();
+
+                    while (nombre == "computadora" && nombre2 == "computadora")
+                    {
+                        Console.WriteLine("ERROR: ningun jugador puede llamarse: Computadora");
+                        nombre = Console.ReadLine();
+                    }
+                    
+                    juego.GraficarTablero();
+                    while (victoria == false)
+                    {
+                        Console.WriteLine("Es el turno de " + nombre);
+                        turno1++;
+                        juego.jugador1();
+                        victoria = juego.Victoria1();
+                        Console.Clear();
+                        juego.GraficarTablero();
+                        if (victoria == true)
+                        {
+                            Console.WriteLine("FELICIDADES" + nombre + "!");
+                            Console.WriteLine("Has ganado en "+turno1+" turnos");
+                            ganador = nombre;
+                            break;
+                        }
+                        Console.WriteLine("Es el turno de " + nombre2);
+                        turno2++;
+                        juego.jugador2();
+                        victoria = juego.Victoria2();
+                        Console.Clear();
+                        juego.GraficarTablero();
+                        if (victoria == true)
+                        {
+                            Console.WriteLine("FELICIDADES" + nombre2+"!");
+                            Console.WriteLine("Has ganado en " + turno2 + " turnos");
+                            ganador = nombre2;
+                            break;
+                        }
+                    }
+                    if (ganador != null)
+                    {
+                        registroGanadores.Add(ganador);
+                    }
+                    break;
+            }
+            break;
+
+        case 2:
+            MostrarRegistroGanadores(registroGanadores);
+            break;
+
+        case 3:
+            Console.WriteLine("Gracias por jugar");
+            break;
+    }
+
+    Console.WriteLine("Desea volver al menu principal?");
+    cont = Console.ReadLine();
+
+} while (cont == "si");
+
+static void MostrarRegistroGanadores(List<string> registroGanadores)
+{
+    Console.Clear();
+    Console.WriteLine("GANADORES:");
+    if (registroGanadores.Count == 0)
+    {
+        Console.WriteLine("No hay ganadores registrados.");
+    }
+    else
+    {
+        foreach (string ganador in registroGanadores)
+        {
+            Console.WriteLine(ganador);
+        }
+    }
+
+}
+
---
+++ proyecto2/juego.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto2
+{
+    internal class juego
+    {
+        
+        int[,] tablero = new int[6,7];
+       
+            public void GraficarTablero()
+        {
+            Console.WriteLine(" 1  2  3  4  5  6  7");
+            for (int f = 0; f < 6; f++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    Console.Write("|");
+                    Console.Write(tablero[f,c]);
+                    Console.Write("|");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        //asignar valor a la ficha
+        public void IgualarTablero()
+        {
+            for (int f = 0; f < 6; f++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    tablero[f, c] = 0;
+                }
+                Console.WriteLine(" ");
+            }
+        }
+      
+        public void computadora()
+        {
+            
+            int numColumna = 0;
+            Random random = new Random();
+            numColumna = random.Next(1, 7);
+            int numFila = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (tablero[i, numColumna - 1] != 0)
+                {
+                    numFila = i - 1;
+                    break;
+                }
+                else if (i == 5)
+                {
+                    numFila = 5;
+                }
+            }
+            tablero[numFila, numColumna - 1] = 2;
+        }
+        public void jugador1()
+        {
+           
+            int numColumna = 0;
+            Console.WriteLine("Ingrese el numero de columna");
+            numColumna = Convert.ToInt32(Console.ReadLine());
+
+            while (numColumna > 7 && numColumna < 0) 
+            {
+                Console.WriteLine("ERROR: el numero de columna debe estar entre 1 y 7, ingreselo nuevamente");
+                numColumna = Convert.ToInt32(Console.ReadLine());
+            }
+            int numFila = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (tablero[i, numColumna - 1] != 0)
+                {
+                    numFila = i - 1;
+                    break;
+                }
+                else if (i == 5)
+                {
+                    numFila = 5;
+                }
+            }
+            tablero[numFila, numColumna - 1] = 1;
+        }
+        public void jugador2()
+        {
+           
+            int numColumna = 0;
+            Console.WriteLine("Ingrese el numero de columna");
+            numColumna = Convert.ToInt32(Console.ReadLine());
+
+            while (numColumna > 7 && numColumna < 0)
+            {
+                Console.WriteLine("ERROR: el numero de columna debe estar entre 1 y 7, ingreselo nuevamente");
+                numColumna = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int numFila = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (tablero[i, numColumna - 1] != 0)
+                {
+                    numFila = i - 1;
+                    break;
+                }
+                else if (i == 5)
+                {
+                    numFila = 5;
+                }
+            }
+            tablero[numFila, numColumna - 1] = 2;
+        }
+
+        public bool Victoria1()
+        //victoria jugador 1
+        {
+            bool victoria = false;
+            //fichas = fichas seguidas
+            int fichas = 0; 
+
+            //validar victoria de forma horizontal 
+            for (int f = 0; f < 6; f++)
+            {
+                for (int c = 0; c < 7; c++)   //revisa la matriz
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 1) //para encontrar una ficha
+                        {
+                            fichas = 1;
+                            for (int i = 1; i < 4; i++) //revisa las posiciones siguientes
+                            {
+                                if (tablero[f, c + i] == 1)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f, c + i] != 1)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+                                return victoria; 
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+
+                }
+            }
+
+            fichas = 0; 
+            //validar victoria de forma horizontal
+            for (int c = 0; c < 6; c++) //columna
+            {
+                for (int f = 0; f < 7; f++) //fila
+                {
+                    //EXAMINA TODA LA MATRIZ
+                    try
+                    {
+                        if (tablero[f, c] == 1)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes
+                            {
+                                if (tablero[f + i, c] == 1)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f + i, c] != 1)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+
+                                return victoria;  //vict
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+
+            //diagonal izquierda a derecha
+
+            for (int f = 0; f < 6; f++) //columna
+            {
+                for (int c = 0; c < 7; c++) //fila
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 1)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes
+                            {
+                                {
+                                    if (tablero[f + i, c + i] == 1)
+                                    {
+                                        fichas++;
+                                    }
+                                    else if (tablero[f + i, c + i] != 1)
+                                    {
+                                        victoria = false;
+                                        break;
+                                    }
+                                }
+                                if (fichas == 4)
+                                {
+                                    victoria = true;
+                                    return victoria;  //victoria
+                                }
+                                break;
+                            }
+
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+            ///diagonal izquierda a derecha
+
+            for (int f = 0; f < 6; f++) //columna
+            {
+                for (int c = 0; c < 7; c++) //fila
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 1)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes 
+                            {
+                                if (tablero[f + i, c - i] == 1)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f + i, c - i] != 1)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+                                return victoria;  //victoria
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+            return victoria;
+        }
+        public bool Victoria2()
+        //victoria jugador 1
+        {
+            bool victoria = false;
+            //fichas = fichas seguidas
+            int fichas = 0;
+
+            //validar victoria de forma horizontal 
+            for (int f = 0; f < 6; f++)
+            {
+                for (int c = 0; c < 7; c++)   //revisa la matriz
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 2) //para encontrar una ficha
+                        {
+                            fichas = 1;
+                            for (int i = 1; i < 4; i++) //revisa las posiciones siguientes
+                            {
+                                if (tablero[f, c + i] == 2)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f, c + i] != 2)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+                                return victoria;
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+
+                }
+            }
+
+            fichas = 0;
+            //validar victoria de forma horizontal
+            for (int c = 0; c < 6; c++) //columna
+            {
+                for (int f = 0; f < 7; f++) //fila
+                {
+                    //EXAMINA TODA LA MATRIZ
+                    try
+                    {
+                        if (tablero[f, c] == 2)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes
+                            {
+                                if (tablero[f + i, c] == 2)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f + i, c] != 2)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+                                return victoria;  //vict
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+
+            //diagonal izquierda a derecha
+
+            for (int f = 0; f < 6; f++) //columna
+            {
+                for (int c = 0; c < 7; c++) //fila
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 2)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes
+                            {
+                                {
+                                    if (tablero[f + i, c + i] == 2)
+                                    {
+                                        fichas++;
+                                    }
+                                    else if (tablero[f + i, c + i] != 2)
+                                    {
+                                        victoria = false;
+                                        break;
+                                    }
+                                }
+                                if (fichas == 4)
+                                {
+                                    victoria = true;
+                                    return victoria;  //victoria
+                                }
+                                break;
+                            }
+
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+            ///diagonal izquierda a derecha
+
+            for (int f = 0; f < 6; f++) //columna
+            {
+                for (int c = 0; c < 7; c++) //fila
+                {
+                    try
+                    {
+                        if (tablero[f, c] == 2)
+                        {
+                            fichas = 1;
+
+                            for (int i = 1; i < 4; ++i) //revisa las posiciones siguientes 
+                            {
+                                if (tablero[f + i, c - i] == 2)
+                                {
+                                    fichas++;
+                                }
+                                else if (tablero[f + i, c - i] != 2)
+                                {
+                                    victoria = false;
+                                    break;
+                                }
+                            }
+                            if (fichas == 4)
+                            {
+                                victoria = true;
+                                return victoria;  //victoria
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        victoria = false;
+                        break;
+                    }
+                }
+            }
+            return victoria;
+        }
+    }
+    
+}
+
